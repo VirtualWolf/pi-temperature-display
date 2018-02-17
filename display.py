fe561be86a4d4fc86ab7a79101c0ea4013d566fe
VirtualWolf <virtualwolf@virtualwolf.org>
2018-02-17 15:20:05 +1100

    Changed font sizes and positions for better readability from a distance.

--- display.py
+++ display.py
@@ -7,13 +7,18 @@ outdoor = requests.get(os.environ['REST_ENDPOINT_OUTDOOR'])
 indoor = requests.get(os.environ['REST_ENDPOINT_INDOOR'])
 text = PapirusTextPos(False) # Don't update the screen immediately
 
-outdoor_formatted = outdoor.json()['temperature'] + '\u00B0 and ' + str(outdoor.json()['humidity']) + '%'
-indoor_formatted = indoor.json()['temperature'] + '\u00B0 and ' + str(indoor.json()['humidity']) + '%'
+outdoor_temperature_formatted = outdoor.json()['temperature'] + '\u00B0'
+indoor_temperature_formatted = indoor.json()['temperature'] + '\u00B0'
 
-text.AddText('Outdoor', 0, 0, size=24, fontPath='/usr/share/fonts/truetype/freefont/FreeSansBold.ttf')
-text.AddText(outdoor_formatted, 0, 24, fontPath='/usr/share/fonts/truetype/freefont/FreeSans.ttf')
+outdoor_humidity_formatted = str(outdoor.json()['humidity']) + '%'
+indoor_humidity_formatted = str(indoor.json()['humidity']) + '%'
 
-text.AddText('Indoor', 0, 52, size=24, fontPath='/usr/share/fonts/truetype/freefont/FreeSansBold.ttf')
-text.AddText(indoor_formatted, 0, 76, fontPath='/usr/share/fonts/truetype/freefont/FreeSans.ttf')
+text.AddText('Outdoor', 0, 0, size=16, fontPath='/usr/share/fonts/truetype/freefont/FreeSansBold.ttf')
+text.AddText(outdoor_temperature_formatted, 0, 16, size=32, fontPath='/usr/share/fonts/truetype/freefont/FreeSans.ttf')
+text.AddText(outdoor_humidity_formatted, 120, 16, size=32, fontPath='/usr/share/fonts/truetype/freefont/FreeSans.ttf')
+
+text.AddText('Indoor', 0, 48, size=16, fontPath='/usr/share/fonts/truetype/freefont/FreeSansBold.ttf')
+text.AddText(indoor_temperature_formatted, 0, 62, size=32, fontPath='/usr/share/fonts/truetype/freefont/FreeSans.ttf')
+text.AddText(indoor_humidity_formatted, 120, 62, size=32, fontPath='/usr/share/fonts/truetype/freefont/FreeSans.ttf')
 
 text.WriteAll()
