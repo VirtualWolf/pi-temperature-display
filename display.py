441538198aa30c236e23cf08a5b8fde4a245bea7
VirtualWolf <virtualwolf@virtualwolf.org>
2018-02-17 15:56:37 +1100

    More readability tweaks.

--- display.py
+++ display.py
@@ -13,12 +13,12 @@ indoor_temperature_formatted = indoor.json()['temperature'] + '\u00B0'
 outdoor_humidity_formatted = str(outdoor.json()['humidity']) + '%'
 indoor_humidity_formatted = str(indoor.json()['humidity']) + '%'
 
-text.AddText('Outdoor', 0, 0, size=16, fontPath='/usr/share/fonts/truetype/freefont/FreeSansBold.ttf')
-text.AddText(outdoor_temperature_formatted, 0, 16, size=32, fontPath='/usr/share/fonts/truetype/freefont/FreeSans.ttf')
-text.AddText(outdoor_humidity_formatted, 120, 16, size=32, fontPath='/usr/share/fonts/truetype/freefont/FreeSans.ttf')
+text.AddText('Outdoor', 0, -2, size=12, fontPath='/usr/share/fonts/truetype/freefont/FreeSansBold.ttf')
+text.AddText(outdoor_temperature_formatted, 0, 3, size=48, fontPath='/usr/share/fonts/truetype/freefont/FreeSans.ttf')
+text.AddText(outdoor_humidity_formatted, 140, 19, size=28, fontPath='/usr/share/fonts/truetype/freefont/FreeSans.ttf')
 
-text.AddText('Indoor', 0, 48, size=16, fontPath='/usr/share/fonts/truetype/freefont/FreeSansBold.ttf')
-text.AddText(indoor_temperature_formatted, 0, 62, size=32, fontPath='/usr/share/fonts/truetype/freefont/FreeSans.ttf')
-text.AddText(indoor_humidity_formatted, 120, 62, size=32, fontPath='/usr/share/fonts/truetype/freefont/FreeSans.ttf')
+text.AddText('Indoor', 0, 47, size=12, fontPath='/usr/share/fonts/truetype/freefont/FreeSansBold.ttf')
+text.AddText(indoor_temperature_formatted, 0, 52, size=48, fontPath='/usr/share/fonts/truetype/freefont/FreeSans.ttf')
+text.AddText(indoor_humidity_formatted, 140, 70, size=28, fontPath='/usr/share/fonts/truetype/freefont/FreeSans.ttf')
 
 text.WriteAll()
